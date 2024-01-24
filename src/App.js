ef668e6da53f9e2a4bab6c60e8b29fc0d2e91a4a
dkag525 <dkag525@gmail.com>
2024-01-24 17:06:02 +0530

    make carousel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const slides = document.querySelectorAll(".slide");
+
+  let counter = 0;
+
+  slides.forEach(
+    // index = keys (index means keys) (slide means classname of each)
+    (slide, index) => {
+      slide.style.left = `${index * 100}%`;
+    }
+  );
+
+  const slideImage = () => {
+    slides.forEach((slide) => {
+      slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+  };
+
+  const goNext = () => {
+    counter++;
+    slideImage();
+  };
+
+  const goPrev = () => {
+    counter--;
+    slideImage();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <img
+          src="https://picsum.photos/id/237/200/300"
+          alt="photo1"
+          className="slide"
+        />
+        <img
+          src="https://picsum.photos/seed/picsum/200/300"
+          alt="photo2"
+          className="slide"
+        />
+        <img
+          src="https://picsum.photos/200/300?grayscale"
+          alt="photo3"
+          className="slide"
+        />
+        <img
+          src="https://picsum.photos/200/300/?blur"
+          alt="photo4"
+          className="slide"
+        />
+      </main>
+      <div className="nav">
+        <button onClick={goPrev}>Prev</button>
+        <button onClick={goNext}>Next</button>
+      </div>
+      <div className="card">
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+    </>
   );
 }
 
